9388c3a64d84b9ad81b9aee0212d276271406c1a
Lancelot <lancelot@Lancelot.local>
2018-04-18 19:41:17 +0100

    first commit

---
+++ Lesson1.py
@@ -0,0 +1,17 @@
+a = [1, 5, 10,12]
+#for i in a:
+#    print(i*i)
+#print(len(a))
+
+#print(list(range(4)))
+
+for i in range(4):
+    print(a[i])
+
+b = [3, 0, 4, 2]
+for i in range(4):
+    print(a[i]*b[i])
+
+print()
+
+print(a*2)
